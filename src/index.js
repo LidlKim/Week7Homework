489d53a91e30672c5c562bb8d186fdeb956df454
LidlKim <hestergrant12@gmail.com>
2022-10-27 13:10:29 +0100

    Updated javascript

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function displayWeatherCondition(response) {
   descriptionElement.innerHTML= response.data.weather[0].description;
 
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = response.data.weather[0].wind;
+  windElement.innerHTML = response.data.current.wind_speed;
 
   let tititle = document.querySelector("#tititle");
   tititle.innerHTML = response.data.name;
